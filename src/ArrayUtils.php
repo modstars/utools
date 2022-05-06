afa675843c1bfde51dffbb13f6034e791010b9c6
宋杨 <945679940@qq.com>
2022-05-06 14:20:55 +0800

    Fix ArrayUtils

--- src/ArrayUtils.php
+++ src/ArrayUtils.php
@@ -18,17 +18,126 @@ class ArrayUtils
      *
      * @param array $array1 数组1
      * @param array $array2 数组2
-     * @return array 交集
-     *
-     * @example
-     * $array1 = array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
-     * $array2 = array("e"=>"red","f"=>"black","g"=>"purple");
-     * 结果: Array ( [a] => red )
+     * @return array
      */
-    public static function intersect($array1, $array2)
+    public static function intersect(array $array1, array $array2)
     {
         return array_values(array_intersect($array1, $array2));
     }
 
+    /**
+     * 去除多维数组中的空值
+     *
+     * @param array $arr 目标数组
+     * @param array $values 去除的值  默认去除  '',null,false,0,'0',[]
+     * @return array
+     */
+    public static function filter(array $arr, array $values = ['', null, false, 0, '0', []]): array
+    {
+        foreach ($arr as $k => $v) {
+            if (is_array($v) && count($v) > 0) $arr[$k] = self::filter($v, $values);
+            foreach ($values as $value) if ($v === $value) {
+                unset($arr[$k]);
+                break;
+            }
+        }
+        return $arr;
+    }
 
+    /**
+     * 二维数组根据字段进行排序
+     *
+     * @param array $array 需要排序的二维数组
+     * @param string $field 排序的字段
+     * @param string $sort 排序顺序标志 SORT_DESC 降序；SORT_ASC 升序
+     * @return mixed
+     */
+    public static function sequence(array $array, string $field, string $sort = 'SORT_DESC'): array
+    {
+        $arrSort = array();
+        foreach ($array as $index => $row) foreach ($row as $key => $value) {
+            $arrSort[$key][$index] = $value;
+        }
+        array_multisort($arrSort[$field], constant($sort), $array);
+        return $array;
+    }
+
+    /**
+     * 转树状结构
+     *
+     * @param array $array 要转换的数据集
+     * @param string $pk key
+     * @param string $parentKey 父级key
+     * @param string $child 子级key
+     * @param int $root 根
+     * @return array
+     */
+    public static function toTree(array $array, string $pk = 'id', string $parentKey = 'pid', string $child = 'child', int $root = 0): array
+    {
+        $tree = array();
+        if (is_array($array)) {
+            $refer = array();
+            foreach ($array as $key => $data) $refer[$data[$pk]] = &$array[$key];
+            foreach ($array as $key => $data) {
+                $parentId = $data[$parentKey];
+                if ($root == $parentId) {
+                    $tree[] = &$array[$key];
+                } else {
+                    if (isset($refer[$parentId])) {
+                        $parent           = &$refer[$parentId];
+                        $parent[$child][] = &$array[$key];
+                    }
+                }
+            }
+        }
+        return $tree;
+    }
+
+    /**
+     * 树状结构转数组
+     *
+     * @param array $tree 树状结构数组
+     * @param string $child 子级key
+     * @param array $list 引用对象 用作遍历
+     * @return array
+     */
+    public static function fromTree(array $tree, string $child = 'child', array &$list = []): array
+    {
+        if(is_array($tree)) {
+            foreach ($tree as $key => $value) {
+                $refer = $value;
+                if(isset($refer[$child])){
+                    unset($refer[$child]);
+                    self::fromTree($value[$child], $child,$list);
+                }
+                $list[] = $refer;
+            }
+        }
+        return $list;
+    }
+
+    /**
+     * 转xml
+     *
+     * @param array $array 数组
+     * @return string
+     */
+    public static function toXml(array $array): string
+    {
+        $xml = "";
+        foreach ($array as $key=>$val) $xml.= is_numeric($val) ? sprintf("<%s>%d</%s>", $key, $val, $key) : sprintf( "<%s><![CDATA[%s]]></%s>", $key, $val, $key);
+        return sprintf("%s%s%s", "<xml>", $xml, "</xml>");
+    }
+
+    /**
+     * xml转数组
+     *
+     * @param string $xml
+     * @return array
+     */
+    public static function fromXml(string $xml): array
+    {
+        libxml_disable_entity_loader(true);
+        return json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+    }
 }
--- tests/ArrayUtilsTest.php
+++ tests/ArrayUtilsTest.php
@@ -13,12 +13,110 @@ use PHPUnit\Framework\TestCase;
 
 class ArrayUtilsTest extends TestCase
 {
-    public function testIntersect(): array
+    private static $array = [
+        ["id" => 1, "pid" => 0, "name" => "张一", "age" => 80],
+        ["id" => 2, "pid" => 1, "name" => "张二", "age" => 60],
+        ["id" => 3, "pid" => 1, "name" => "张三", "age" => 55],
+        ["id" => 4, "pid" => 0, "name" => "李一", "age" => 75],
+        ["id" => 5, "pid" => 4, "name" => "李二", "age" => 55]
+    ];
+
+    private static $tree_array = [
+        ["id" => 1, "pid" => 0, "name" => "张一", "age" => 80, "child" => [
+            ["id" => 2, "pid" => 1, "name" => "张二", "age" => 60],
+            ["id" => 3, "pid" => 1, "name" => "张三", "age" => 55]
+        ]],
+        ["id" => 4, "pid" => 0, "name" => "李一", "age" => 75, "child" => [
+            ["id" => 5, "pid" => 4, "name" => "李二", "age" => 55]
+        ]]
+    ];
+
+    /**
+     * 数组-交集
+     */
+    public function testIntersect()
     {
-        $array1 = [1, 2, 3];
-        $array2 = [3, 4, 5];
-        $array3 = ArrayUtils::intersect($array1, $array2);
-        $this->assertCount(1, $array3, "数组-合并-测试不通过");
-        return $array3;
+        $this->assertSame([3], ArrayUtils::intersect([1, 2, 3], [3, 4, 5]), "数组-交集-测试不通过");
+    }
+
+    /**
+     * 数组-过滤
+     *
+     * @dataProvider filterProvider
+     * @param $param
+     * @param $result
+     */
+    public function testFilter($param, $result)
+    {
+        $this->assertSame($result, ArrayUtils::filter($param), "数组-过滤-测试不通过");
+    }
+
+    /**
+     * 转树状结构
+     *
+     */
+    public function testToTree()
+    {
+        $this->assertCount(2, ArrayUtils::toTree(self::$array), "数组-转树结构-测试不通过");
+    }
+
+    /**
+     * 树状结构转数组
+     */
+    public function testFromTree()
+    {
+        $this->assertCount(5, ArrayUtils::fromTree(self::$tree_array), "数组-树状结构转数组-测试不通过");
+    }
+
+    /**
+     * 转xml
+     */
+    public function testToXml()
+    {
+        $array = ["amount"=>101, "name"=>"工具类"];
+        $this->assertIsString(ArrayUtils::toXml($array), "数组-转XML-测试不通过");
+    }
+
+    /**
+     * xml转数组
+     */
+    public function testFromXml()
+    {
+        $xml = "<xml><amount>101</amount><name><![CDATA[工具类]]></name></xml>";
+        $this->assertIsArray(ArrayUtils::fromXml($xml), "数组-数组转XML-测试不通过");
+    }
+
+    /**
+     * 二维数组排序
+     */
+    public function testSequence() {
+        $res = ArrayUtils::sequence(self::$array, "age");
+        $this->assertEquals(2, $res[2]["id"], "数组-排序-测试不通过");
+    }
+
+    /**
+     * 数组-过滤-测试数据
+     *
+     * @return array
+     */
+    public function filterProvider(): array
+    {
+        return [
+            [
+                ["some word", '', null, false, 0, '0', []],
+                ["some word"]
+            ],
+            [
+                [
+                    ['name' => "name1", 'age' => 18, 'hobby' => "reading"],
+                    ['name' => "name2", 'age' => 19, 'hobby' => ""],
+                    []
+                ],
+                [
+                    ['name' => "name1", 'age' => 18, 'hobby' => "reading"],
+                    ['name' => "name2", 'age' => 19],
+                ]
+            ]
+        ];
     }
 }
